09a10cc1521e5de4894e1a2b05fd452672b8803f
Hongmiao Yu <hy1709@nyu.edu>
2023-05-25 21:58:22 -0700

    change to recursive

--- random_genarator.py
+++ random_genarator.py
@@ -8,52 +8,52 @@ fanout_min = 4
 fanout_max = 6
 op_max = 10
 
-def generate_tree(level, name, root, file):
-    global_count = 0
-    level_stack = []
-    next_stack = []
-    level_stack.append(root)
 
-    for i in range(level):
-        while len(level_stack) != 0 :
-            node = level_stack.pop()
-            for j in range(random.randrange(fanout_min, fanout_max+1)):
-                global_count = global_count + 1
-                node_name = name + "_" + str(global_count)
-                file.write(node + " " + node_name + "\n")
-                next_stack.append(node_name)
+# generateTree(levelMin, levelMax, parent, fanoutMin, fanoutMax):
+#   n = generate_node(...)
+#   if parent:
+#     print(f"{parent} - {n}")
+#   if randrange(levelMin, levelMax) > 0: 
+#     fanout = randrange(fanoutMin, fanoutMax)
+#     for i in range(0, fanout):
+#         generateTree(levelMin - 1, levelMax - 1, n, , fanoutMin, fanoutMax);
 
-        level_stack = next_stack
-        next_stack = []
-    
+def generate_tree(levelMin, levelMax, parent, fanoutMin, fanoutMax, name_prefix, file, global_count):
+    random.seed(datetime.now().timestamp())
+    n = name_prefix + str(global_count)
+    if parent:
+        file.write(f"{parent} {n}\n")
+    if random.randrange(levelMin, levelMax) > 0: 
+        fanout = random.randrange(fanoutMin, fanoutMax)
+        for i in range(0, fanout):
+            global_count = generate_tree(levelMin - 1, levelMax - 1, n, fanoutMin, fanoutMax, name_prefix, file, global_count+1)
     return global_count
 
 
-random.seed(datetime.now().timestamp())
 
-
-
-admin_root = "Admin_0"
-incident_root = "Incident_0"
 
 file = open("random_init.txt", "w")
 
-admin_count = generate_tree(random.randrange(level_min,level_max+1), "Admin", admin_root, file)
-incident_count = generate_tree(random.randrange(level_min,level_max+1), "Incident", incident_root, file)
+admin_count = generate_tree(level_min, level_max, "Admin_0", fanout_min, fanout_max, "Admin_", file, 1)
+incident_count = generate_tree(level_min, level_max, "Incident_0", fanout_min, fanout_max, "Incident_", file, 1)
 
 file.close()
 
+random.seed(datetime.now().timestamp())
 op_count = random.randrange(1, op_max+1)
 
 file = open("random_update.txt", "w")
 for i in range(op_count):
 
+    
     admin_random = random.randrange(admin_count)
     incident_random = random.randrange(incident_count)
 
     admin_node = "Admin_" + str(admin_random)
     incident_node = "Incident_" + str(incident_random)
-    file.write("add " + incident_node + " " + admin_node + "\n")
-    file.write("del " + incident_node + " " + admin_node + "\n")
+    if i%2 == 0:
+        file.write("add " + incident_node + " " + admin_node + "\n")
+    else:
+        file.write("del " + incident_node + " " + admin_node + "\n")
     
 file.close()
